900eb5a6c140b5eb557034c4024438bf3e02dde2
Vitalii <vit.okushko@gmail.com>
2022-07-05 20:19:14 +0300

    Добавлена связь между анимацией Героя и скриптом передвижения

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,38 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private float _powerJump;
+    [SerializeField] private float _jumpForce;
+    
+    private float _speedCurrent = 0;
     private static float _ranSpeedMulti = 2;
+    private Animator _animator;
 
-    void Update()
+    private void Start()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-_speed * Time.deltaTime, 0, 0);
-        }
+        _animator = GetComponent<Animator>();
+    }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(_speed * Time.deltaTime, 0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(0, _powerJump * Time.deltaTime, 0);
-        }
-
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            _speed = _speed / _ranSpeedMulti;
-        }
+    private void Update()
+    {
+        _speedCurrent = 0;
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             _speed *= _ranSpeedMulti;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _speed /= _ranSpeedMulti;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _speedCurrent = -_speed;
+            
+            if(transform.localScale.x > 0)
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _speedCurrent = _speed;
+
+            if (transform.localScale.x < 0)
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
+
+        transform.Translate(_speedCurrent * Time.deltaTime, 0, 0);
+        _animator.SetFloat("Speed", Math.Abs(_speedCurrent));
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0, _jumpForce * Time.deltaTime, 0);
+            _animator.SetTrigger("Jump");
         }
     }
 }
